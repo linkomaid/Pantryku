52f2e1bf0cd8a031527c9550dbe9600cc243fbe0
Clyde.Dev <72ports@gmail.com>
2026-06-15 21:42:46 +0700

    feat: Adding up LED and Buzzer on ino

--- arduino/main.ino
+++ arduino/main.ino
@@ -5,89 +5,156 @@
 
 const char* ssid = "YOUR_WIFI_NAME";
 const char* password = "YOUR_WIFI_PASSWORD";
-
 const char* serverUrl = "http://YOUR_LOCAL_IP:3000/api/sensor";
 
 Adafruit_AHTX0 aht;
-
 String deviceID;
 
-unsigned long lastSend = 0;
-const long interval = 3000;
+const int redLED = 4;
+const int yellowLED = 16;
+const int greenLED = 17;
+const int whiteLED = 2;
+const int buzzerPin = 13;
+const int buttonPin = 5;
+
+bool lastButtonState = HIGH;
 
 void connectWiFi() {
     WiFi.begin(ssid, password);
-    Serial.print("Connecting to WiFi");
 
+    Serial.print("Connecting WiFi");
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
     }
 
-    Serial.println("\nWiFi connected!");
+    Serial.println("\nWiFi connected");
     Serial.println(WiFi.localIP());
+}
+
+String checkFreshness(float temp, float humidity) {
+    if (temp >= 22 && temp <= 28 && humidity >= 40 && humidity <= 60)
+        return "Segar";
+
+    if (temp >= 20 && temp <= 32 && humidity >= 30 && humidity <= 75)
+        return "Kurang Segar";
+
+    return "Tidak Segar";
+    }
+
+void resetLED() {
+    digitalWrite(redLED, LOW);
+    digitalWrite(yellowLED, LOW);
+    digitalWrite(greenLED, LOW);
+    digitalWrite(whiteLED, LOW);
+}
+
+
+void beep(int duration) {
+    digitalWrite(buzzerPin, HIGH);
+    delay(duration);
+    digitalWrite(buzzerPin, LOW);
+}
+
+void sendData(float temp, float humidity, String status) {
+    if (WiFi.status() != WL_CONNECTED) return;
+
+    HTTPClient http;
+    http.begin(serverUrl);
+    http.addHeader("Content-Type", "application/json");
+
+    String payload = "{";
+    payload += "\"deviceID\":\"" + deviceID + "\",";
+    payload += "\"temperature\":" + String(temp) + ",";
+    payload += "\"humidity\":" + String(humidity) + ",";
+    payload += "\"status\":\"" + status + "\"";
+    payload += "}";
+
+    int response = http.POST(payload);
+
+    Serial.print("API Response: ");
+    Serial.println(response);
+
+    http.end();
 }
 
 void setup() {
     Serial.begin(115200);
     delay(1000);
 
+    pinMode(redLED, OUTPUT);
+    pinMode(yellowLED, OUTPUT);
+    pinMode(greenLED, OUTPUT);
+    pinMode(whiteLED, OUTPUT);
+
+    pinMode(buzzerPin, OUTPUT);
+    pinMode(buttonPin, INPUT_PULLUP);
+
+    digitalWrite(buzzerPin, LOW);
+    resetLED();
+
     uint64_t chipid = ESP.getEfuseMac();
-    deviceID = String((uint32_t)(chipid >> 32), HEX) + String((uint32_t)chipid, HEX);
+    deviceID = String((uint32_t)(chipid >> 32), HEX) +
+                String((uint32_t)chipid, HEX);
 
     Wire.begin();
 
     if (!aht.begin()) {
         Serial.println("AHT sensor not found!");
-        while (1) delay(10);
+        while (1);
     }
 
     connectWiFi();
 }
 
 void loop() {
-    if (WiFi.status() != WL_CONNECTED) {
-        Serial.println("WiFi lost. Reconnecting...");
-        connectWiFi();
-    }
+    bool buttonState = digitalRead(buttonPin);
 
-    if (millis() - lastSend > interval) {
-        lastSend = millis();
+    if (lastButtonState == HIGH && buttonState == LOW) {
 
-        sensors_event_t humidity, temp;
-        aht.getEvent(&humidity, &temp);
+        Serial.println("Button pressed!");
 
-        float t = temp.temperature;
-        float h = humidity.relative_humidity;
+        resetLED();
+        digitalWrite(whiteLED, HIGH);
+        beep(100);
+        delay(2000);
+        digitalWrite(whiteLED, LOW);
+
+        delay(3000);
+
+        sensors_event_t humidityEvent, tempEvent;
+        aht.getEvent(&humidityEvent, &tempEvent);
+
+        float temp = tempEvent.temperature;
+        float humidity = humidityEvent.relative_humidity;
 
         Serial.print("Temp: ");
-        Serial.println(t);
+        Serial.println(temp);
 
         Serial.print("Humidity: ");
-        Serial.println(h);
+        Serial.println(humidity);
 
-    if (WiFi.status() == WL_CONNECTED) {
-            HTTPClient http;
+        String status = checkFreshness(temp, humidity);
+        Serial.println(status);
 
-            http.begin(serverUrl);
-            http.addHeader("Content-Type", "application/json");
+        resetLED();
 
-            String payload = "{";
-            payload += "\"deviceID\":\"" + deviceID + "\",";
-            payload += "\"temperature\":" + String(t) + ",";
-            payload += "\"humidity\":" + String(h);
-            payload += "}";
+        beep(150);
 
-            int httpResponseCode = http.POST(payload);
+        delay(200);
 
-            Serial.print("HTTP Response: ");
-            Serial.println(httpResponseCode);
-
-            if (httpResponseCode < 0) {
-                Serial.println("Failed to send data");
-            }
-
-            http.end();
+        if (status == "Segar") {
+        digitalWrite(greenLED, HIGH);
+        } else if (status == "Kurang Segar") {
+        digitalWrite(yellowLED, HIGH);
+        } else {
+        digitalWrite(redLED, HIGH);
         }
+        sendData(temp, humidity, status);
+
+        delay(1000);
+        resetLED();
     }
+
+    lastButtonState = buttonState;
 }
